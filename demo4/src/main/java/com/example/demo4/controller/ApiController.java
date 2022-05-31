601f7592609d0e9ecdd91ef70b09bdd8baac2a47
devWonil <102580743+devWonil@users.noreply.github.com>
2022-05-31 17:25:28 +0900

    Response와 MIME TYPE

---
+++ demo4/src/main/java/com/example/demo4/controller/ApiController.java
@@ -0,0 +1,39 @@
+package com.example.demo4.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo4.dto.User;
+
+@RestController
+@RequestMapping("/api")
+public class ApiController {
+
+	// text/plain <- contentType : text/plain
+	@GetMapping("/text")
+	public String text(@RequestParam String account) {
+		return account;
+	}
+	
+	// application/json
+	@PostMapping("/json")
+	// request -> Object Mapper -> object -> method
+	public User json(@RequestBody User user) {
+		System.out.println(user.getName());
+		return user;
+	}
+	
+	// 202, 200 -> 응답을 돌려 줄때 응답 코드를 지정해서 보낼 수 있다
+	@PutMapping("/put")
+	public ResponseEntity<User> put(@RequestBody User user) {
+		// ResponseEntity를 통해서 status, header 등을 설정할 수 있다.
+		return ResponseEntity.status(HttpStatus.OK).body(user);
+	}
+}
---
+++ demo4/src/main/java/com/example/demo4/dto/User.java
@@ -0,0 +1,74 @@
+package com.example.demo4.dto;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+
+/*
+ * {
+ * 		"name" : "kkk",
+ * 		"age" : 30,
+ * 		"phone_number" : "010-1234-5678",
+ * 		"address" : "부산시 진구 중앙대로 668"
+ * }
+ * */
+@JsonNaming(value = PropertyNamingStrategies.SnakeCaseStrategy.class)
+@JsonInclude(JsonInclude.Include.NON_NULL) // null 값을 내려주고 싶지 않을 때 사용한다.
+public class User {
+	//@JsonProperty("phone_number")
+	private String name;
+	private String phoneNumber;
+	private String address;
+	private int age;
+	private int idNumber;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	
+	public int getIdNumber() {
+		return idNumber;
+	}
+
+	public void setIdNumber(int idNumber) {
+		this.idNumber = idNumber;
+	}
+
+	@Override
+	public String toString() {
+		return "User [name=" + name + ", phoneNumber=" + phoneNumber + ", address=" + address + ", age=" + age
+				+ ", idNumber=" + idNumber + "]";
+	}
+
+	
+
+}

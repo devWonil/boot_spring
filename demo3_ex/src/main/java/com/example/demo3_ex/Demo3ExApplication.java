1c13629d084ac1f311c58b6e9e540037aeae60cb
devWonil <102580743+devWonil@users.noreply.github.com>
2022-05-31 17:24:28 +0900

    연습

---
+++ demo3_ex/src/main/java/com/example/demo3_ex/Demo3ExApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo3_ex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo3ExApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demo3ExApplication.class, args);
+	}
+
+}
---
+++ demo3_ex/src/test/java/com/example/demo3_ex/Demo3ExApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo3_ex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo3ExApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

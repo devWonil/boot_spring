33d779d35e17d8b382534b7e857387cdb7d6a6b3
devWonil <102580743+devWonil@users.noreply.github.com>
2022-06-07 17:26:06 +0900

    예외처리

---
+++ exception_ex1/src/main/java/com/example/jsptest/advice/GlobalControllerAdvice.java
@@ -0,0 +1,41 @@
+package com.example.jsptest.advice;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+//@RestControllerAdvice(basePackages = "com.example.jsptest.controller") 특정 패키지에서만 동작
+//@ControllerAdvice 뷰리졸브, data (MessageConverter)
+@RestControllerAdvice // Global
+public class GlobalControllerAdvice {
+
+	// 모든 예외상황을 처리하고 싶을 때 사용
+	
+	@ExceptionHandler(value = Exception.class)
+	public ResponseEntity<?> exception(Exception e){
+		
+		System.out.println("=====================");
+		System.out.println(e.getClass());
+		System.out.println(e.getMessage());
+		System.out.println("=====================");
+		
+		return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("");
+	}
+	
+	// 특정 예외 상황에서는 다른 응답 처리를 하고 싶을 때
+	// MissingServletRequestParameterException
+	@ExceptionHandler(value = MissingServletRequestParameterException.class)
+	public ResponseEntity<?> missingServletRequestParameterException(MissingServletRequestParameterException e) {
+		System.out.println("MissingServletRequestParameterException 동작!!");
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+	}
+	
+	@ExceptionHandler(value = MethodArgumentNotValidException.class)
+	public ResponseEntity<?> methodArgumentNotValidException(MethodArgumentNotValidException e){
+
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+	}
+}
---
+++ exception_ex2/src/main/java/com/example/exception2/advice/ApiControllerAdvice.java
@@ -0,0 +1,98 @@
+package com.example.exception2.advice;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.validation.ConstraintViolationException;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import com.example.exception2.dto.CustomError;
+
+@RestControllerAdvice
+public class ApiControllerAdvice {
+	
+	@ExceptionHandler(value = Exception.class)
+	public ResponseEntity<?> exeption(Exception e){
+		System.out.println("예외 발생");
+		System.out.println("=====================");
+		System.out.println(e.getClass());
+		System.out.println("=====================");
+		System.out.println(e.getMessage());
+		System.out.println("=====================");
+		System.out.println(e.getCause());
+		
+		return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("");
+	}
+	// 예외 발생 및 처리 get 방식일 때 파라미터 없을 경우 발생
+	// MissingServletRequestParameterException
+	@ExceptionHandler(value = MissingServletRequestParameterException.class)
+	public ResponseEntity<?> missingServletRequestParameterException(MissingServletRequestParameterException e){
+		System.out.println("MissingServletRequestParameterException 동작");
+		
+		String fieldName = e.getParameterName();
+		String fieldType = e.getParameterType();
+		String invalidValue = e.getMessage();
+		
+		System.out.println("fieldName : " + fieldName);
+		System.out.println("fieldType : " + fieldType);
+		System.out.println("invalidValue : " + invalidValue);
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(fieldName + "을 입력해야 합니다");
+	}
+	
+	@ExceptionHandler(value = MethodArgumentNotValidException.class)
+	public ResponseEntity<?> methodArgumentNotValidException(MethodArgumentNotValidException e){
+		System.out.println("methodArgumentNotValidException 예외 발생");
+		
+		// 한번에 전부 들어온다
+		List<CustomError> errorList = new ArrayList<>();
+		BindingResult bindingResult = e.getBindingResult();
+		bindingResult.getAllErrors().forEach(action -> {
+			// action
+			FieldError field = (FieldError) action;
+			
+			String fieldName = field.getField();
+			String message = field.getDefaultMessage();
+			
+			CustomError customError = new CustomError();
+			customError.setField(fieldName);
+			customError.setMessage(message);
+			errorList.add(customError);
+			
+//			System.out.println("action : " + action.toString());
+		});
+		
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorList);
+	}
+	
+	@ExceptionHandler(value = ConstraintViolationException.class)
+	public ResponseEntity<?> constraintViolationException(ConstraintViolationException e){
+		// 동시에 확인 validation
+		System.out.println("ConstraintViolationException 예외 발생");
+		List<CustomError> errorList = new ArrayList<>();
+		
+		e.getConstraintViolations().forEach(error -> {
+			String strField = error.getPropertyPath().toString();
+			int index = strField.indexOf(".");
+			
+			String fieldName = strField.substring(index + 1);
+			String message = error.getMessage();
+			
+			CustomError customError = new CustomError();
+			customError.setField(fieldName);
+			customError.setMessage(message);
+			errorList.add(customError);
+			
+//			System.out.println(strField);
+		});
+		
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorList);
+	}
+}
